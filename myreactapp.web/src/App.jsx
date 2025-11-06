f48c06e231d0b3aa7c43c3113456371765f9669d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-11-06 13:04:35 -0500

    Updating frontend to get weather info from the api

--- myreactapp.web/src/App.jsx
+++ myreactapp.web/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [weatherData, setWeatherData] = useState([]);
+
+  const getWeather = async () =>{
+    fetch("/api/weatherforecast")
+    .then(response => response.json()
+    .then(json => setWeatherData(json)))
+    .catch(error => console.error("Error fetching weather data:", error));
+  }
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  const contents = weatherData === undefined
+    ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
+    : <table id="weatherTable">
+        <thead>
+          <tr><th>Date</th>
+          <th aria-label="Temperature in Celsius">Temp. (C)</th>
+          <th aria-label="Temperature in Fahrenheit">Temp. (F)</th>
+          <th aria-label="Weather forecast summary">Summary</th></tr>
+        </thead>
+        <tbody>
+          {weatherData.map(forecast =>
+            <tr key={forecast.date}>
+              <td>{new Date(forecast.date).toLocaleDateString()}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Weather forecast</h1>
+
+    <p>This component demonstrates fetching data from the server.</p>
+    {contents}
     </>
   )
 }
