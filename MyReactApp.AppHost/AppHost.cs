fc134ab4594991efbe25b829b3101f3abfc361a8
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-11-06 12:42:13 -0500

    Updating AppHost.cs

--- MyReactApp.AppHost/AppHost.cs
+++ MyReactApp.AppHost/AppHost.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var apiService = builder.AddProject<Projects.MyReactApp_api>("apiservice");
+
+builder.AddViteApp("frontend", "../myreactapp.web")
+        .WithReference(apiService)
+        .WaitFor(apiService);
+
 builder.Build().Run();
